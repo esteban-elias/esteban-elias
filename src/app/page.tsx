35e63dbb98c1c7f6597dcc0b8b53748f49abab54
esteban-elias <esteban.elias@proton.me>
2024-01-07 17:08:41 -0300

    Color texts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,10 +11,10 @@ export default function Home() {
           />
         </div>
         <div className="flex flex-col items-center sm:items-start">
-          <h1 className="font-montserrat font-bold text-2xl sm:text-4xl mt-4 sm:mt-0">
+          <h1 className="font-montserrat font-bold text-sky-950 text-2xl sm:text-4xl mt-4 sm:mt-0">
             Esteban Castillo
           </h1>
-          <p className="max-w-sm text-sm sm:text-lg text-justify hyphens-auto sm:text-left mt-4">
+          <p className="max-w-sm text-sky-900 text-sm sm:text-lg text-justify hyphens-auto sm:text-left mt-4">
             Analista Programador y estudiante de Ingeniería Informática
             — Desarrollo Web.
           </p>
@@ -37,7 +37,7 @@ export default function Home() {
 
       {/* Projects section */}
       <section className="flex flex-col w-full grow mt-8 sm:mt-12">
-        <h2 className="font-montserrat font-bold text-2xl">
+        <h2 className="font-montserrat font-bold text-sky-900 text-xl sm:text-2xl">
           Proyectos
         </h2>
         {proyects.map((proyect, index) => (
@@ -54,19 +54,19 @@ export default function Home() {
             />
             <div className="sm:w-1/2">
               <div className="mt-2">
-                <h3 className="font-montserrat font-bold text-xl mt-6">
+                <h3 className="font-montserrat font-bold text-sky-950 text-xl mt-6">
                   {proyect.name}
                 </h3>
-                <p className="mt-3">{proyect.description}</p>
-                <div className="mt-3">
+                <p className="text-sky-900 mt-3">{proyect.description}</p>
+                <div className="font-bold text-sky-900 mt-3">
                   {proyect.techStack.join(' - ')}
                 </div>
               </div>
               <div className="flex gap-4 mt-5">
-                <button className="bg-gray-300 px-4 py-2 rounded">
+                <button className="bg-sky-100 border border-1 border-sky-300 font-bold text-sky-950 px-4 py-2 rounded shadow-lg active:shadow-none hover:bg-sky-300">
                   <a href={proyect.demoUrl}>Demo</a>
                 </button>
-                <button className="bg-gray-300 px-4 py-2 rounded">
+                <button className="bg-sky-100 border border-1 border-sky-300 font-bold text-sky-950 px-4 py-2 rounded shadow-lg active:shadow-none hover:bg-sky-300">
                   <a href={proyect.repoUrl}>Repo</a>
                 </button>
               </div>
